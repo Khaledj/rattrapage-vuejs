e8a9b32c3e597c1375d6110c8bf91059bad3e9c2
khaled.djhehiche <d-khaled@hotmail.fr>
2018-07-18 17:10:33 +0200

    creation du composant projet et changer de style (actif/non actif)

---
+++ src/Projet.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-bind:class="{ 'projet':!isActive}">
+    <h2>  {{n}}</h2>
+    <img v-bind:src = 'picture'> </img>
+    <p v-if="isActive">projet actif</p>
+    <p v-else>projet non actif</p>
+  </div>
+</template>
+
+<script>
+    export default {
+        props : [],
+        name: 'projet',
+        data : function() {
+          return {
+            _id : '5b3e3da861f2d927949fa8da',
+            isActive : false,
+            picture : 'http://placehold.it/32x32',
+            n : 'PUSHCART',
+            creation : 'Tue Feb 25 1975 21:31:07 GMT+0100 (Central European Standard Time)'
+          }
+        }
+    }
+</script>
+
+<style scoped>
+.projet {
+  border : solid;
+  background-color : red;
+  width : 250px;
+  margin:auto;
+
+}
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Projet from './Projet.vue'
+
+
+Vue.component('projet',Projet)
 
 new Vue({
   el: '#app',
